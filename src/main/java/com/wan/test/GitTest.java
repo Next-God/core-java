0994b0a3655ada30b2e201ba199ab5a0be0d5a17
万亚明 <641567183@qqcom>
2019-07-23 07:53:14 +0800

    commit

--- src/main/java/com/wan/test/GitTest.java
+++ src/main/java/com/wan/test/GitTest.java
@@ -2,6 +2,6 @@ package com.wan.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hey,branch changed");
+        System.out.println("Hey,master changed");
     }
 }
